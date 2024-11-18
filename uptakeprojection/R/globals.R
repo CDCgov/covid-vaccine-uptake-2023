ed65f4c59995fd3b5f748b219c408aff47ce96b0
Scott Olesen <ulp7@cdc.gov>
2024-11-18 16:44:30 -0500

    add globals

---
+++ uptakeprojection/R/globals.R
@@ -0,0 +1,9 @@
+globalVariables(c(
+    "indicator_name", "time_period", "time_start", "time_end", "time_year", "ci_half_width_95pct",
+    "coninf_95", "dataset", "demographic_level", "demographic_name", "estimate",
+    "estimate_boosted", "estimate_booster", "estimate_maybe", "estimate_no", "estimate_primary",
+    "estimate_yes", "geographic_name", "geography", "group_category", "group_name",
+    "indicator_category", "indicator_category_label", "indicator_category_sd", "lci", "method",
+    "nd_weekly_estimate", "obs_slope", "proj_uptake", "sd", "sd_boosted", "sd_booster", "sd_maybe",
+    "sd_no", "sd_primary", "sd_yes", "season", "time_type", "uci", "week_ending"
+))
